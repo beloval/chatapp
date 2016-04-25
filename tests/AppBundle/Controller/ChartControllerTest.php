26bc4043ee0c6c559878288978cf358e386edfd8
Aleksandr Belov <belov@jump.kg>
2016-04-24 23:10:23 -0400

    last changes

---
+++ tests/AppBundle/Controller/ChartControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 4/24/16
+ * Time: 9:51 PM
+ */
+
+namespace Tests\AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
+
+class ChartControllerTest extends WebTestCase
+{
+    public function set_up($conn){
+
+
+    }
+
+    public function testShowPost()
+    {
+        $this->asserts(true);
+    }
+
+    public function testShowGet()
+    {
+    }
+
+
+
+} 
